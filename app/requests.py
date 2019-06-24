e5a34c8cfb11286d3ef6b0f79c9ac5cb3c77db21
Beryl Onyancha <berylonyancha@gmail>
2019-06-24 15:19:47 +0300

    create functions to get sources and process the results

--- app/requests.py
+++ app/requests.py
@@ -12,5 +12,43 @@ def configure_request(app):
     api_key = app.config['NEWS_API_KEY']
     news_url = app.config['NEWS_API_BASE_URL']
     article_url = app.config['ARTICLE_API_BASE_URL']
+def get_sources():
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_sources_url = news_url.format(api_key)
+  
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results = None
+
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_results(sources_results_list)
+
+    return sources_results
+
+def process_results(sources_list):
+    '''
+    Function  that processes the news result and transform them to a list of Objects
+    Args:
+        sources_list: A list of dictionaries that contain news details
+    Returns :
+        news_results: A list of news objects
+    '''
+    sources_results = []
+    for sources_item in sources_list:
+        id = sources_item.get('id')
+        name = sources_item.get('name')
+        description = sources_item.get('description')
+        url = sources_item.get('url')
+
+        source_object= Sources(id, name, description,url)
+        sources_results.append(source_object)
+
+    return sources_results
+
 
 
