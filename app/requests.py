569f47074d985e8ed3d9d5577ae1fdbd8a0fac45
Beryl Onyancha <berylonyancha@gmail>
2019-06-24 15:20:47 +0300

    create functions to get articles and process the results

--- app/requests.py
+++ app/requests.py
@@ -12,6 +12,51 @@ def configure_request(app):
     api_key = app.config['NEWS_API_KEY']
     news_url = app.config['NEWS_API_BASE_URL']
     article_url = app.config['ARTICLE_API_BASE_URL']
+
+def get_articles(id):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_articles_url = article_url.format(id, api_key)
+
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_data = url.read()
+        get_articles_response = json.loads(get_articles_data)
+
+        articles_results = None
+
+        if get_articles_response['articles']:
+            articles_results_list = get_articles_response['articles']
+            articles_results = process_articles(articles_results_list)
+
+    return articles_results
+
+
+def process_articles(articles_list):
+
+    '''
+    This is a function that processes the articles result and transform them to a list of articles
+    Args:
+        articles_list: A list of dictionaries that contain articles
+    Returns :
+        articles_results: A list of article objects
+    ''' 
+    articles_results = []
+    for articles_item in articles_list:
+        id = articles_item.get('id')
+        name = articles_item.get('name')
+        title = articles_item.get('title')
+        url = articles_item.get('url')
+        image = articles_item.get('urlToImage')
+        content = articles_item.get('content')
+        publishedAt = articles_item.get('publishedAt')
+
+
+        if image:
+            article_object= Articles(id, name, title, url, image, content, publishedAt)
+            articles_results.append(article_object)
+
+    return articles_results
 def get_sources():
     '''
     Function that gets the json response to our url request
@@ -49,6 +94,7 @@ def process_results(sources_list):
         sources_results.append(source_object)
 
     return sources_results
+    
 
 
 
